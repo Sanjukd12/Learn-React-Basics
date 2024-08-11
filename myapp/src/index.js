298142dc7d691a4814764807aa0867eeb7a1905b
Sanjeev Kumar <108565766+Sanjukd12@users.noreply.github.com>
2024-08-11 14:01:17 +0530

    Multi const passed in function

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -1,10 +1,37 @@
-import react from 'react';
-import ReactDom from 'react-dom/client';
+import React from "react";
+import ReactDom from "react-dom/client";
+import "./index.css";
 
-function Greeting(){
-    return <h2>My First Component</h2>;
-}
+const BookList = () => {
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+};
 
-const root = ReactDom.createRoot(document.getElementById('root'))
+const Book = () => {
+  return (
+    <article className="book">
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
 
-root.render(<Greeting />);
+const Image = () => (
+  <img
+    src="https://m.media-amazon.com/images/I/51X4eBEqyqL._SY445_SX342_.jpg"
+    alt="It Ends with us"
+  />
+);
+const Title = () => <h2>It Ends with Us</h2>;
+const Author = () => {
+  return <h4>Colleen Hoover</h4>;
+};
+const root = ReactDom.createRoot(document.getElementById("root"));
+
+root.render(<BookList />);
