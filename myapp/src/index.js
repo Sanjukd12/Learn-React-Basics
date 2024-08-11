1442e914000f2b4683a8f2e1a584275e96c12aab
Sanjeev Kumar <108565766+Sanjukd12@users.noreply.github.com>
2024-08-11 13:11:34 +0530

    Created first function after removing boiler plate and created new own function

---
+++ myapp/src/index.js
@@ -0,0 +1,10 @@
+import react from 'react';
+import ReactDom from 'react-dom/client';
+
+function Greeting(){
+    return <h2>My First Component</h2>;
+}
+
+const root = ReactDom.createRoot(document.getElementById('root'))
+
+root.render(<Greeting />);
